50227afd7703951a44f34bf04a03dbb2e9b80dab
Bernhard Huber <bernhard.huber@in-tech.at>
2017-08-30 09:20:17 +0200

    feat (python-google-drive): successful oauth2 configuration

--- python/trials/google_drive_connection.py
+++ python/trials/google_drive_connection.py
@@ -3,8 +3,16 @@ from pydrive.drive import GoogleDrive
 
 def connect_to_google_drive():
     gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()
+    #gauth.LocalWebserverAuth()
 
     drive = GoogleDrive(gauth)
+    # print all files on the drive
+    file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    for file1 in file_list:
+        print('title: %s, id: %s' % (file1['title'], file1['id']))
+    # Create GoogleDriveFile instance with title 'Hello.txt'.
+    file1 = drive.CreateFile({'title': 'Hello.txt'})
+    file1.Upload()  # Upload the file.
+    print('title: %s, id: %s' % (file1['title'], file1['id']))
 
 connect_to_google_drive()
