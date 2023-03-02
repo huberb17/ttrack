567769468d209510caeb97401d3d2f44ba6ee7f8
Bernhard Huber <bernhard.huber@in-tech.com>
2023-03-02 12:10:49 +0100

    chore: replace __init__ with static builder

--- backend/ttrack/persistence/expense.py
+++ backend/ttrack/persistence/expense.py
@@ -1,13 +1,34 @@
+from __future__ import annotations
 class Expense:
     """Implements the Expense object."""
 
-    def __init__(self, category, km_factor, route):
-        """The initializer method of the class."""
-        self._id = route._id.encode('utf-8')
-        self._category = category.encode('utf-8')
-        self._date = route._date.encode('utf-8')
-        self._text = route._comment
-        self._value = float(route._route_distance) * km_factor
+    _id = 0
+    _category = ''
+    _date = ''
+    _text = ''
+    _value = 0
+
+    @staticmethod
+    def build_from_values(category, km_factor, route) -> Expense:
+        """Build object from distinct values."""
+        expense = Expense()
+        expense._id = route._id
+        expense._category = category
+        expense._date = route._date
+        expense._text = route._comment
+        expense._value = float(route._route_distance) * km_factor
+        return expense
+
+    @staticmethod
+    def build_from_json(json_data):
+        """Build object from json data."""
+        expense = Expense()
+        expense._id = json_data['id']
+        expense._category = json_data['category']
+        expense._date = json_data['date']
+        expense._text = json_data['text_for_report']
+        expense._value = json_data['value']
+        return expense
 
     def convert_to_db_object(self):
         data = {
