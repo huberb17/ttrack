b3da539034726b5596525f0831bcd4020a7ad977
Bernhard Huber <bernhard.huber@in-tech.com>
2023-03-02 12:03:39 +0100

    chore: replace __init__ with static builder

--- backend/ttrack/persistence/income.py
+++ backend/ttrack/persistence/income.py
@@ -1,12 +1,31 @@
+from __future__ import annotations
 class Income:
     """Implements the Income object."""
 
-    def __init__(self, id, date, text, value):
-        """The initializer method of the class."""
-        self._id = id.encode('utf-8')
-        self._date = date.encode('utf-8')
-        self._text = text.encode('utf-8')
-        self._value = value
+    _id = 0
+    _date = ''
+    _text = ''
+    _value = 0
+
+    @staticmethod
+    def build_from_values(id, date, text, value) -> Income:
+        """Build object from distinct values."""
+        income = Income()
+        income._id = id
+        income._date = date
+        income._text = text
+        income._value = value
+        return income
+
+    @staticmethod
+    def build_from_json(json_data)->Income:
+        """Build object from json data."""
+        income = Income()
+        income._id = json_data['id']
+        income._date = json_data['date']
+        income._text = json_data['text_for_report']
+        income._value = json_data['value']
+        return income
 
     def convert_to_db_object(self):
         data = {
